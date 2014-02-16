e937f348dfc0e0fe127b330ab712643d17c61e43
keeeeenw <wangxiaotian2007@gmail.com>
2014-02-16 14:35:19 -0600

    finished working on controller and model

--- flaskr.py
+++ flaskr.py
@@ -24,6 +24,69 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+    with app.app_context(): #app context is not created yet, so we create it by hand
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def get_db():
+    """
+    Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.route('/')
+def show_games():
+    db = get_db()
+    cur = db.execute('select title, num_player from game order by id desc')
+    games = cur.fetchall()
+    return render_template('show_games.html', games = games)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    # use ? to specify query parameters
+    db.execute('insert into entries (title, num_player) values (?, ?)',
+                [request.form['title'], request.form['num_player']])
+    db.commit()
+    flash('New game was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None) #don't need to check it the key exist
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    Closes the database again at the end of the request.
+    """
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close();
+
 if __name__ == '__main__':
     app.run() # fireup the server
 
