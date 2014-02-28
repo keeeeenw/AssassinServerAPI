1a1f4b85aa2344bdb85f8215cd4e422ee9ff3aee
keeeeenw <wangxiaotian2007@gmail.com>
2014-02-28 00:40:12 -0600

    OAuth is partly working

--- application/view.py
+++ application/view.py
@@ -2,6 +2,9 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, jsonify
 from application import app
 from models import Game
+from google.appengine.api import users
+from decorators import login_required, admin_required
+
 
 def api_verif(dev_key):
     if dev_key == 't6ra1M77Ei80b35LeV5I55EN7c':
@@ -23,7 +26,7 @@ def show_games():
     for g in gs:
         games.append({'title':str(g.title), 'num_player': int(g.num_player)})
 
-    print(games)
+    # print(games)
 
     return render_template('show_games.html', games = games)
 
@@ -34,7 +37,8 @@ def list_games():
         if not api_verif(key):
             abort(401)
     else:
-        if not session.get('logged_in'):
+        user = users.get_current_user()
+        if not user:
             abort(401)
     # Getting all the games
     gs = Game.all()
@@ -47,8 +51,10 @@ def list_games():
     return jsonify(**games)
 
 @app.route('/add', methods=['POST'])
+@login_required
 def add_game():
-    if not session.get('logged_in'):
+    user = users.get_current_user()
+    if not user:
         abort(401)
 
     #db = get_db()
@@ -65,20 +71,55 @@ def add_game():
     flash('New game was successfully posted')
     return redirect(url_for('show_games'))
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_games'))
-    return render_template('login.html', error=error)
+    providers = {
+        'Google'   : 'https://www.google.com/accounts/o8/id',
+        'Yahoo'    : 'yahoo.com',
+        'MySpace'  : 'myspace.com',
+        'AOL'      : 'aol.com',
+        'MyOpenID' : 'myopenid.com'
+        # add more here
+    }
+    loginInfo = {} 
+    loginInfo['greeting'] = "Not Signed In"
+    user = users.get_current_user()
+    if user:  # signed in already
+        loginInfo['greeting'] = ('Hello %s!') % (
+            user.nickname())
+    else:     # let user choose authenticator
+        for name, uri in providers.items():
+            provider = {'url':users.create_login_url(federated_identity=uri),
+                        'name': name}
+            print(provider)
+            if 'providers' not in loginInfo:
+                loginInfo['providers'] = [provider]
+            else:
+                loginInfo['providers'].append(provider)
+
+    #if request.method == 'POST':
+    #    user = users.get_current_user()
+    #    if user:
+    #        greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+    #                    (user.nickname(), users.create_logout_url('/')))
+    #    else:
+    #        error = "Not Signed In"
+    #        greeting = ('<a href="%s">Sign in or register</a>.' %
+    #                    users.create_login_url('/'))
+
+    #error = None
+    #if request.method == 'POST':
+    #    if request.form['username'] != app.config['USERNAME']:
+    #        error = 'Invalid username'
+    #    elif request.form['password'] != app.config['PASSWORD']:
+    #        error = 'Invalid password'
+    #    else:
+    #        session['logged_in'] = True
+    #        flash('You were logged in')
+    #        return redirect(url_for('show_games'))
+    # return render_template('login.html', error=greeting)
+
+    return render_template('login.html', loginInfo=loginInfo)
 
 @app.route('/logout')
 def logout():
