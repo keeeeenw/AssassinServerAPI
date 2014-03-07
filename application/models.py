04d37e39dd6e453d6b8c51155fbea6a75816a2ec
keeeeenw <wangxiaotian2007@gmail.com>
2014-03-06 22:37:21 -0600

    Adding all the database entities for the first iteration

--- application/models.py
+++ application/models.py
@@ -8,11 +8,35 @@ from google.appengine.ext import db
 import hashlib
 import uuid
 
-
+"""
+Here are the models
+"""
 class Game(db.Model):
     title = db.StringProperty(required=True)
-    num_player = db.IntegerProperty(required=True)
+    num_player = db.IntegerProperty(required=True) #maximum number of player
+    creation_date = db.DateTimeProperty(auto_now_add=True)
+    start_time = db.DateTimeProperty()
+    end_time = db.DateTimeProperty()
 
+
+class User(db.Model):
+    __table_name__ = 'user'
+    username = db.StringProperty(required=True)
+    password_hash = db.StringProperty(required=True)
+    creation_date = db.DateTimeProperty(auto_now_add=True)
+    email = db.StringProperty(required=True)
+
+class GamePlayers(db.Model):
+    join_date = db.DateTimeProperty(required=True)
+    start_time = db.DateTimeProperty()
+    end_time = db.DateTimeProperty()
+    player = db.ReferenceProperty(User, collection_name="players")
+    game = db.ReferenceProperty(Game, collection_name="games")
+    isFinished = db.BooleanProperty()
+
+"""
+Helper functions
+"""
 
 def hash_password(password):
     # salt = uuid.uuid4().hex
@@ -25,13 +49,9 @@ def verify_password(password, hashed_password):
     # return password == hashlib.sha256(salt.encode() + password.encode()).hexdigest()
     return password == hashed_password
 
-
-class User(db.Model):
-    __table_name__ = 'user'
-    username = db.StringProperty(required=True)
-    password_hash = db.StringProperty(required=True)
-
-
+"""
+Let's create some fake data
+"""
 def bootstrap():
     """
         Adding bootstrap model objects to the database
@@ -52,7 +72,6 @@ def cleanup():
     for game in Game.all():
         if isinstance(game, Game):
             game.delete()
-
 
 cleanup()
 if Game.all().count() == 0 or User.all().count() == 0: #run bootstrap if there is no data
