f74aacb0f079e73e124eb08fd2f33aaa2a4aeda5
yulunli <yulun.li2@gmail.com>
2014-04-05 23:50:37 -0500

    Changed player and game field in GamePlayer to be required, and also changed their collection_name

--- application/models.py
+++ application/models.py
@@ -13,18 +13,16 @@ Here are the models
 """
 
 
-class Game(db.Model): #each class is a table, each field is a column
+class Game(db.Model):  # each class is a table, each field is a column
     title = db.StringProperty(required=True) #required=True means not nullable
     num_player = db.IntegerProperty(required=True)  # maximum number of player
-    start_up = db.StringListProperty(required=True)
-    current_state = db.StringListProperty(required=True)
     creation_date = db.DateTimeProperty(auto_now_add=True)
     start_time = db.DateTimeProperty()
     end_time = db.DateTimeProperty()
 
 
-class User(db.Model):
-    __table_name__ = 'user'
+class Player(db.Model):
+    # __table_name__ = 'user'
     username = db.StringProperty(required=True)
     password_hash = db.StringProperty(required=True)
     creation_date = db.DateTimeProperty(auto_now_add=True)
@@ -35,9 +33,9 @@ class GamePlayer(db.Model):
     join_date = db.DateTimeProperty(auto_now_add=True)
     start_time = db.DateTimeProperty()
     end_time = db.DateTimeProperty()
-    player = db.ReferenceProperty(User, collection_name="game_players")
-    game = db.ReferenceProperty(Game, collection_name="game_players") #a 1-many relationship
-    isFinished = db.BooleanProperty()
+    player = db.ReferenceProperty(Player, required=True, collection_name="games") #, collection_name='game_players111')
+    game = db.ReferenceProperty(Game, required=True, collection_name="players")  # a 1-many relationship
+    # is_finished = db.BooleanProperty()
 
 
 class TargetHistory(db.Model):
@@ -73,42 +71,63 @@ def bootstrap():
     """
         Adding bootstrap model objects to the database
     """
-    game1 = Game(title="BattleRoyale", num_player=5, start_up=["admin", "u1", "u2"], current_state=["admin", "u1", "u2"])
+    game1 = Game(title="BattleRoyale", num_player=5)
     game1.put()
-    game2 = Game(title="SQLAssassin", num_player=20, start_up=["admin", "u1", "u2"], current_state=["admin", "u1", "u2"])
+    game2 = Game(title="SQLAssassin", num_player=20)
     game2.put()
-    game3 = Game(title="g_test", num_player=3, start_up=["admin", "u1", "u2"], current_state=["admin", "u1", "u2"])
+    game3 = Game(title="g_test", num_player=3)
     game3.put()
 
+    # Seed an admin user, which equivalent to a normal user as for now
+    user = Player(username="admin", password_hash=hash_password("default"))
+    user.put()
 
-    user1 = User(username="admin", password_hash=hash_password("default"))
-    user1.put()
-
+    # Seed 10 users
     for i in range(0, 10):
-        user = User(username="u" + str(i), password_hash=hash_password("p" + str(i)))
-        user.put()
+        player = Player(username="u" + str(i), password_hash=hash_password("p" + str(i)))
+        player.put()
 
-    # gameplayer1 = GamePlayer()
-    # gameplayer1.game = game1
-    # gameplayer1.player = user2
+
+    p10 = Player(username="u10", password_hash=hash_password("p10"))
+    p10.put()
+    p11 = Player(username="u11", password_hash=hash_password("p11"))
+    p11.put()
+    p12 = Player(username="u12", password_hash=hash_password("p12"))
+    p12.put()
+
+    # Seed a game
+
+    # names = ["u1", "u2", "u3", "u10"]
+    # for n in names:
+    #     p = Player.gql("username=" + n).get()
+    #     GamePlayer(game=game1, player=p).put()
+        #     print("*************************** seeded gp: " + str(p.username))
+        # else:
+        #     print("*************************** temp not a player: " + str(type(p)))
+
+
+
+    gameplayer1 = GamePlayer(game=game1, player=p10)
     # gameplayer1.isFinished = False
-    # gameplayer1.put()
-    #
-    # gameplayer2 = GamePlayer()
-    # gameplayer2.game = game1
-    # gameplayer2.player = user3
+    gameplayer1.put()
+
+    gameplayer2 = GamePlayer(game=game1, player=p11)
     # gameplayer2.isFinished = False
-    # gameplayer2.put()
-    #
-    # gameplayer3 = GamePlayer()
-    # gameplayer3.game = game2
-    # gameplayer3.player = user2
+    gameplayer2.put()
+
+    gameplayer3 = GamePlayer(game=game1, player=p12)
     # gameplayer3.isFinished = False
-    # gameplayer3.put()
+    gameplayer3.put()
+
+    temp_player = Player.get_by_id(5821089435353088)
+
+    print("**********************************" + temp_player.username)
+
+
 
 def cleanup():
-    for user in User.all():
-        if isinstance(user, User):
+    for user in Player.all():
+        if isinstance(user, Player):
             user.delete()
     for game in Game.all():
         if isinstance(game, Game):
@@ -117,7 +136,8 @@ def cleanup():
         if isinstance(gp, GamePlayer):
             gp.delete()
 
+
 # Using this because before_fist_request only registers one function
-def set_up_wrapped():
+if Player.all() is None:
     cleanup()
     bootstrap()
