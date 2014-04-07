818651f9909cbbd8dc738eb5e2c37b5588dfea59
yulunli <yulun.li2@gmail.com>
2014-04-07 02:11:14 -0500

    1. The passwords are hashed, actually now 2. The TargetHistory model is updated 3. bootstrap() is updated to address multiple issues

--- application/models.py
+++ application/models.py
@@ -4,9 +4,9 @@ models.py
 App Engine datastore models
 
 """
+from random import shuffle
 from google.appengine.ext import db
 import hashlib
-import uuid
 
 """
 Here are the models
@@ -14,7 +14,7 @@ Here are the models
 
 
 class Game(db.Model):  # each class is a table, each field is a column
-    title = db.StringProperty(required=True) #required=True means not nullable
+    title = db.StringProperty(required=True)  # required=True means not nullable
     num_player = db.IntegerProperty(required=True)  # maximum number of player
     creation_date = db.DateTimeProperty(auto_now_add=True)
     start_time = db.DateTimeProperty()
@@ -22,7 +22,6 @@ class Game(db.Model):  # each class is a table, each field is a column
 
 
 class Player(db.Model):
-    # __table_name__ = 'user'
     username = db.StringProperty(required=True)
     password_hash = db.StringProperty(required=True)
     creation_date = db.DateTimeProperty(auto_now_add=True)
@@ -33,16 +32,18 @@ class GamePlayer(db.Model):
     join_date = db.DateTimeProperty(auto_now_add=True)
     start_time = db.DateTimeProperty()
     end_time = db.DateTimeProperty()
-    player = db.ReferenceProperty(Player, required=True, collection_name="games") #, collection_name='game_players111')
+    player = db.ReferenceProperty(Player, required=True, collection_name="games")  # collection_name='game_players111')
     game = db.ReferenceProperty(Game, required=True, collection_name="players")  # a 1-many relationship
-    # is_finished = db.BooleanProperty()
 
 
-class TargetHistory(db.Model):
-    killer = db.ReferenceProperty(GamePlayer, collection_name="killerplayers") #killer can be many players
-    target = db.ReferenceProperty(GamePlayer, collection_name="targetedplayers")#target can be many players
+class GameHistory(db.Model):
+    killer = db.ReferenceProperty(Player, required=True, collection_name="killer_history")  # killer can be many players
+    target = db.ReferenceProperty(Player, required=True, collection_name="target_history")  # target can be many players
+    game = db.ReferenceProperty(Game, required=True,
+                                collection_name="game_history")  # target can be many players    filter('killer =', killer).
     assign_date = db.DateTimeProperty()
-    isComplete = db.BooleanProperty()
+    is_complete = db.BooleanProperty(required=True)
+    complete_time = db.DateTimeProperty()
 
 
 """
@@ -51,15 +52,14 @@ Helper functions
 
 
 def hash_password(password):
-    # salt = uuid.uuid4().hex
-    # return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ":" + salt
-    return password
+    return hashlib.md5(password).hexdigest()
 
 
 def verify_password(password, hashed_password):
-    # password, salt = hashed_password.split(':')
-    # return password == hashlib.sha256(salt.encode() + password.encode()).hexdigest()
-    return password == hashed_password
+    if hashlib.md5(password) == hashed_password:
+        return password == hashed_password
+    else:
+        return False
 
 
 """
@@ -71,58 +71,33 @@ def bootstrap():
     """
         Adding bootstrap model objects to the database
     """
-    game1 = Game(title="BattleRoyale", num_player=5)
-    game1.put()
-    game2 = Game(title="SQLAssassin", num_player=20)
-    game2.put()
-    game3 = Game(title="g_test", num_player=3)
-    game3.put()
 
     # Seed an admin user, which equivalent to a normal user as for now
     user = Player(username="admin", password_hash=hash_password("default"))
     user.put()
 
-    # Seed 10 users
+    # Seed 10 users and a game
+
+    player_ids = []
     for i in range(0, 10):
         player = Player(username="u" + str(i), password_hash=hash_password("p" + str(i)))
-        player.put()
+        player_ids.append(player.put().id())
 
-
-    p10 = Player(username="u10", password_hash=hash_password("p10"))
-    p10.put()
-    p11 = Player(username="u11", password_hash=hash_password("p11"))
-    p11.put()
-    p12 = Player(username="u12", password_hash=hash_password("p12"))
-    p12.put()
-
-    # Seed a game
-
-    # names = ["u1", "u2", "u3", "u10"]
-    # for n in names:
-    #     p = Player.gql("username=" + n).get()
-    #     GamePlayer(game=game1, player=p).put()
-        #     print("*************************** seeded gp: " + str(p.username))
-        # else:
-        #     print("*************************** temp not a player: " + str(type(p)))
-
-
-
-    gameplayer1 = GamePlayer(game=game1, player=p10)
-    # gameplayer1.isFinished = False
-    gameplayer1.put()
-
-    gameplayer2 = GamePlayer(game=game1, player=p11)
-    # gameplayer2.isFinished = False
-    gameplayer2.put()
-
-    gameplayer3 = GamePlayer(game=game1, player=p12)
-    # gameplayer3.isFinished = False
-    gameplayer3.put()
-
-    temp_player = Player.get_by_id(5821089435353088)
-
-    print("**********************************" + temp_player.username)
-
+    player_names = player_ids[0:5]
+    game_title = "Java"
+    shuffle(player_names)
+    players_to_join = [Player.get_by_id(player_id) for player_id in player_names]
+    new_game = Game(title=game_title, num_player=len(players_to_join))
+    new_game.put()
+    for i in range(-1, len(players_to_join) - 1):
+        killer = players_to_join[i]
+        target = players_to_join[i + 1]
+        if killer is not None and target is not None:
+            GamePlayer(game=new_game, player=killer).put()
+            GameHistory(killer=killer, target=target, game=new_game, is_complete=False).put()
+        else:
+            cleanup()
+            print("Error in seeding!")
 
 
 def cleanup():
@@ -135,9 +110,12 @@ def cleanup():
     for gp in GamePlayer.all():
         if isinstance(gp, GamePlayer):
             gp.delete()
+    for gh in GameHistory.all():
+        if isinstance(gh, GameHistory):
+            gh.delete()
 
 
 # Using this because before_fist_request only registers one function
-if Player.all() is None:
+if Player.all().get() is None:
     cleanup()
     bootstrap()
