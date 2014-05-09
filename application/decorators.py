095672f5959d7a2f6711e78ac50b060521290539
yulunli <yulun.li2@gmail.com>
2014-05-08 23:09:52 -0500

    not using jsonp

--- application/decorators.py
+++ application/decorators.py
@@ -19,23 +19,6 @@ def login_required(func):
     return decorated_view
 
 
-def jsonp(func):
-    """Wraps JSONified output for JSONP requests."""
-
-    @wraps(func)
-    def decorated_function(*args, **kwargs):
-        callback = request.args.get('callback', False)
-        if callback:
-            data = str(func(*args, **kwargs).data)
-            content = str(callback) + '(' + data + ')'
-            mimetype = 'application/javascript'
-            return current_app.response_class(content, mimetype=mimetype)
-        else:
-            return func(*args, **kwargs)
-
-    return decorated_function
-
-
 def support_jsonp(f):
     """Wraps JSONified output for JSONP"""
 
